c400204a03cfbfb03bcb207562e48c8f754f0350
Gary <gengliangyu2008@gmail.com>
2019-05-19 23:10:06 +0800

    remove unnecessary files

--- json-files/validate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-py validateJsonFile.py
-
-pause
--- json-files/validateJsonFile.py
+++
@@ -1,9 +0,0 @@
-import json
-
-## Test json file
-def validateJsonContent(json_filename):
-	with open(json_filename, mode="r", encoding="UTF-8") as json_file:
-		data = json.loads(json_file.read())
-
-if __name__ == '__main__':
-   validateJsonContent("modules-part.json")
